80b91ae0cb2081fcbb5b90e6de3f78a16fef1632
billxu <billxu0521@gmail.com>
2024-09-06 14:51:52 +0800

    upate

---
+++ src/components/NoFaceman.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useAnimations } from '@tresjs/cientos'
+import { useLoader } from '@tresjs/core'
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader'
+
+
+const { scene: model, animations } = await useLoader(GLTFLoader, 'nofaceman.glb')
+
+const { actions } = useAnimations(animations, model)
+
+const currentAction = ref(actions.Greeting)
+
+
+
+</script>
+
+<template>
+  <primitive :object="model" />
+</template>
